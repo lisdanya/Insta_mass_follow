67d2afd717b13d4d9fff79286e32326f46786af7
Daniel <52752918+lisdanya@users.noreply.github.com>
2020-07-20 01:20:04 +0300

    Added write to file filtered person, time sleep

--- inst_sub2.py
+++ inst_sub2.py
@@ -12,7 +12,7 @@ class Subscribe:
         self.file = file_sub
         self.log = login
         self.passwd = passw
-        self.browser = webdriver.Chrome("D:\Programming\instagram\chromedriver.exe")
+
 
     def find_element(self, element):
         try:
@@ -27,13 +27,16 @@ class Subscribe:
     # link_person = "https://www.instagram.com/d_n_sl/"
 
     def login(self):
+        self.browser = webdriver.Chrome("D:\Programming\instagram\chromedriver.exe")
         self.browser.get("https://www.instagram.com/")
         self.browser.get("https://www.instagram.com/accounts/login/")
-        time.sleep(3)
+        time.sleep(5)
         self.browser.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input').send_keys(self.log)
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input').send_keys(
+            self.log)
         self.browser.find_element_by_xpath(
-            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys(self.passwd)
+            '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys(
+            self.passwd)
         self.browser.find_element_by_xpath(
             '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]').click()
         time.sleep(3)
@@ -50,6 +53,8 @@ class Subscribe:
         subscribed = '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[2]/span/span[1]/button'
         send_message = '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/button'
         post = '//*[@id="react-root"]/section/main/div/header/section/ul/li[1]/span/span'
+        no_person = '//*[@id="react-root"]/section/main/div/p'
+
         # posts1 = '//*[@id="react-root"]/section/main/div/div[4]/article/div[1]/div[2]/div[1]/div'
         # posts2 = '//*[@id="react-root"]/section/main/div/div[3]/article/div[1]/div[2]/div[1]/div'
         count_pers = 0
@@ -58,8 +63,15 @@ class Subscribe:
         for person in self.persons:
             count_pers += 1
             self.browser.get(person)
-            time.sleep(2)
-            time.sleep(3)
+            time.sleep(1)
+            if self.find_element(no_person) == 1:
+                print(str(count_pers) + ") Not available")
+                # try:
+                #     if self.browser.find_element_by_xpath(no_person).text== "The link you followed may be broken, or the page may have been removed. ":
+                #         print(str(count_pers) + ") Not available")
+                # except NoSuchElementException:
+                #     print("/\/\/\/ Error : 1 /\/\/\/")
+                continue
             posts = self.browser.find_element_by_xpath(post).text
             posts = re.sub(r'\s', '', posts)
             if int(posts) == 0:
@@ -84,17 +96,46 @@ class Subscribe:
             else:
                 filtered += 1
                 self.filtered_persons.append(person)
-                print(str(count_pers) + ") Added")
+                print(str(count_pers) + ") Added  " + str(filtered))
             if filtered == self.lim * 24:
                 break
         return self.filtered_persons
 
+    def write_filtered(self):
+        self.file = open(self.log + '.txt', 'w')
+        for i in self.filtered_persons:
+            self.file.write(i)
+            # self.file.write("\n")
+        # self.file.close()
+
+    def del_subed(self):
+        # cou = 0
+        # for line in self.file:
+        #     cou += 1
+        #     line==''
+        #     if cou == self.counter:
+        #         break
+        self.file = open(self.log + '.txt', 'r')
+        temp = []
+        for line in self.file:
+            temp.append(line)
+        count = 0
+        self.file.close()
+        self.file = open(self.log + '.txt', 'w')
+        for i in temp:
+            count += 1
+            if count >= self.counter:
+                self.file.write(i)
+                # self.file.write('\n')
+        self.file.close()
+
     def subs(self):
-        counter = 0
-        for person in self.filtered_persons:
-            counter += 1
+        self.counter = 0
+        self.file = open(self.log + '.txt', 'r')
+        for person in self.file:
+            self.counter += 1
             self.browser.get(person)
-            time.sleep(2)
+            time.sleep(4)
             stor = '//*[@id="react-root"]/section/main/div/div[3]'
             if self.find_element(stor) == 1:
                 self.browser.find_element_by_xpath(
@@ -102,19 +143,24 @@ class Subscribe:
             else:
                 self.browser.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/div[2]/article/div/div/div[1]/div[1]').click()
-            time.sleep(2)
+            time.sleep(3)
             self.browser.find_element_by_xpath(
                 '/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button').click()
             self.browser.find_element_by_xpath(
                 '/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button').click()
-            time.sleep(1)
-            if counter == self.lim:
-                time.sleep(3600)
+            time.sleep(2)
+            if self.counter == self.lim:
+                self.del_subed()
+                self.browser.close()
+                time.sleep(30)
+                self.login()
+                # time.sleep(3600)
 
 
-yourzayka = Subscribe(30, 'D:\Programming\instagram\_anime96.txt', '__yourzayka', '55555dan')
+
+yourzayka = Subscribe(10, 'D:\Programming\instagram\\1234', '__yourzayka', '55555dan')
 yourzayka.login()
 yourzayka.read_from_file()
 yourzayka.filter_person()
+yourzayka.write_filtered()
 yourzayka.subs()
-
